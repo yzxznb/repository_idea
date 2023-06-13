13b6749f84a92a8d5bddbf8edd19d30172ea5413
yzxznb <2329318813@qq.com>
2023-06-13 16:03:40 +0800

    修改GitServlet类

--- src/main/java/com/xy/git_dome/GitServlet.java
+++ src/main/java/com/xy/git_dome/GitServlet.java
@@ -1,4 +1,19 @@
 package com.xy.git_dome;
 
-public class GitServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/git")
+public class GitServlet extends HttpServlet {
+    @Override
+    protected  void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    protected  void doPost(HttpServletRequest request, HttpServletResponse response){
+        doGet(request, response);
+    }
 }
